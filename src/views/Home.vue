453eaacb5301403ee88a7432c26c35928e3ec56a
sidratariq <sidratariq48@gmail.com>
2019-05-25 06:05:15 +0000

    text and colors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
-<template>
+  <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home page</h1>
+
+    
+    <p class="pink white--text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde alias odio doloribus atque nam porro, laudantium quas quis neque quae nisi, magnam expedita veritatis suscipit officiis ipsum nemo dolor esse. </p>
+    <p class="pink lighten-4 red--text text-darken-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde alias odio doloribus atque nam porro, laudantium quas quis neque quae nisi, magnam expedita veritatis suscipit officiis ipsum nemo dolor esse. </p>
+
+    <h1 class="display-4">Massive text</h1>
+    <h4 class="display-1">Smaller display</h4>
   </div>
-</template>
+  </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  <script>
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+    export default {
+      components: {
+      }
+    }
+  </script>
